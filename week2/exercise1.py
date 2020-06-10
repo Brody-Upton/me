8e8e6b4ce2845bf9d269fd5aaf705baeafa3bd1a
Brody <brodyupton@gmail.com>
2020-06-10 11:36:20 +1000

    Completion of exercise 1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -11,19 +11,30 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+#I think this will create a list named "some_words" which contains "what", "does", "this", "line", "do"
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
+#It created a list 
 
+#I think this will recall a specific word in the list and print it as some text
 for word in some_words:
     print(word)
+#It was actually some sort of loop which printed ALL the objects in the list
 
+#I think this will do the exact same as the last command
 for x in some_words:
     print(x)
+#The command printed all the objects in the list but with a different notation
 
+#I think this will print the list in its command line notation rather than the individual words
 print(some_words)
+#The command printed the command line syntax of some_words
 
+#Since some_words is 5 objects long, I think it will definitely print some words contains more than 3 words
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
+#the command printed some words contains more than 3 words
 
+#I think this will produce a tupel with 6 objects which describes my computer
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -33,3 +44,4 @@ def usefulFunction():
     print(platform.uname())
 
 usefulFunction()
+#It produced a tupel of facts about the computer I'm using (processor, OS, version, node etc.)
