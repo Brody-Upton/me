d1f325fc9864483d2b5af32ddd67a82559a5f8ab
Brody <brodyupton@gmail.com>
2020-06-11 22:05:22 +1000

    exercise 3 process

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,10 +7,7 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    if a_number % 2 == 0
-    return "False"
-
-    elif return "True"
+    return (a_number % 2) != 0
 
 
 def fix_it(moves=True, should_move=True):
@@ -28,7 +25,12 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    if moves and not should_move:
+        return "Duct Tape"
+    elif not moves and should_move:
+        return "WD-40"
+    else :
+        return "No Problem"
 
 
 def loops_1a():
@@ -38,7 +40,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    stars = []
+    for i in range(10):
+        stars.append("*")
+    return stars
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -48,7 +53,11 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    a_list = []
+
+    for i in range(number_of_items):
+        a_list.append(symbol)
+    return a_list
 
 
 def loops_2():
@@ -69,7 +78,11 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    star_list =[]
+
+    for i in range(10):
+        star_list.append(loops_1a())
+    return star_list
 
 
 def loops_3():
@@ -93,7 +106,8 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    something = map(loops_2,range(10))
+    return something
 
 
 def loops_4():
