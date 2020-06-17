7ccd9318755ca166a878e022dbd784e2e0a73702
Brody <brodyupton@gmail.com>
2020-06-17 10:37:09 +1000

    exercise 1 progress

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,15 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    num_list = []
+
+    x = start
+    while x < stop:
+        print(x)
+        num_list.append(x)
+        x = x + step
+    
+    return num_list
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +28,7 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    return list(range(start, stop, step))
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +37,12 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
+    range_list = []
+    y = start
+    while y < stop:
+        range_list.append(y)
+        y = y + 2
+    return range_list
 
 
 def stubborn_asker(low, high):
