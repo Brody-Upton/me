bc2ad4d359f2df3776ccbd6c7e0fc596ae070bbc
Brody <brodyupton@gmail.com>
2020-07-29 09:26:53 +1000

    exam progress

--- week8/exercise1.py
+++ week8/exercise1.py
@@ -11,28 +11,28 @@ import time
 
 def string_please() -> str:
     """Returns a string, anything you like."""
-    return None
+    return "None"
 
 
 def list_please() -> list:
     """Returns a list, anything you like."""
-    return None
+    return list("None")
 
 
 def dictionary_please() -> dict:
     """Returns a dictionary, anything you like."""
-    return None
+    return {"First_name": "joe", "Last_name": "mama"}
 
 
 def is_it_5(some_number) -> bool:
     """Returns True if the argument passed is 5, otherwise returns False."""
-    well_is_it = None
+    well_is_it = bool(some_number)
     return well_is_it
 
 
 def take_five(some_number) -> int:
     """Subtracts 5 from some_number."""
-    return None
+    return some_number - 5
 
 
 def greet(name="Towering Timmy"):
